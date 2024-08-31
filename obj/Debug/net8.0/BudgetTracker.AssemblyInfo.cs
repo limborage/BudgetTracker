dcb2d04018a03e1e8f10a8580638d035024c6312
Lee <lee@asgsolutions.co.za>
2024-08-31 14:09:24 +0200

    budget-tracker-fix-expense-index * Fix budget tracker expenses page index styling

--- obj/Debug/net8.0/BudgetTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/BudgetTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dccb96cbfff048d43762ff5d2654b2e0772eef44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0390a2945d57a4650505080776e2dd7e774366b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
